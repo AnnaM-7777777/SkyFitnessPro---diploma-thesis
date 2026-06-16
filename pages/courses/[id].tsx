aa88d169e9d46186642f4c4a40b4ee1048c1badc
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2026-06-17 01:06:36 +0300

    Сделан адаптив страницы курса под мобильную версию и планшет

--- pages/courses/[id].tsx
+++ pages/courses/[id].tsx
@@ -214,19 +214,20 @@ export default function CoursePage() {
 
                 {/* Описание курса */}
                 <section className={styles.description}>
-                    <p className={styles.description__text}>
-                        <span className={styles.description__title}>{title} – </span>
+                    <p className={styles.description__info}>
+                        <span className={styles.description__boldedTerm}>{title} – </span>
                         {course.description || "Описание курса будет здесь"}
                     </p>
                 </section>
 
                 {/* Блок "Подойдет для вас, если" */}
                 <section className={styles.conditions}>
-                    <h2 className={styles.title}>Подойдет для вас, если:</h2>
+                    <h2 className={styles.conditions__title}>Подойдет для вас, если:</h2>
 
                     <div className={styles.conditions__block}>
                         <div className={styles.condition}>
                             <span className={styles.condition__number}>1</span>
+
                             <p className={styles.condition__description}>
                                 Давно хотели попробовать, но не решались начать
                             </p>
@@ -251,12 +252,25 @@ export default function CoursePage() {
                 {/* Направления */}
                 {course.directions?.length && (
                     <section className={styles.directions}>
-                        <h2 className={styles.title}>Направления</h2>
+                        <h2 className={styles.directions__title}>Направления</h2>
 
                         <div className={styles.directions__block}>
                             {course.directions.map((dir, i) => (
                                 <div key={i} className={styles.direction}>
-                                    <span className={styles.direction__span}>⯌</span>
+                                    <svg
+                                        className={styles.direction__span}
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 20 20"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M9.21373 1.11751C9.3702 0.454433 9.44843 0.122896 9.57424 0.0482295C9.68259 -0.0160765 9.81741 -0.0160765 9.92576 0.0482295C10.0516 0.122896 10.1298 0.454434 10.2863 1.11751L11.0497 4.35302C11.3337 5.55636 11.4757 6.15803 11.7843 6.64596C12.0571 7.07744 12.4226 7.44285 12.854 7.71574C13.342 8.02432 13.9436 8.1663 15.147 8.45025L18.3825 9.21373C19.0456 9.3702 19.3771 9.44843 19.4518 9.57424C19.5161 9.68259 19.5161 9.81741 19.4518 9.92576C19.3771 10.0516 19.0456 10.1298 18.3825 10.2863L15.147 11.0497C13.9436 11.3337 13.342 11.4757 12.854 11.7843C12.4226 12.0571 12.0571 12.4226 11.7843 12.854C11.4757 13.342 11.3337 13.9436 11.0497 15.147L10.2863 18.3825C10.1298 19.0456 10.0516 19.3771 9.92576 19.4518C9.81741 19.5161 9.68259 19.5161 9.57424 19.4518C9.44843 19.3771 9.3702 19.0456 9.21373 18.3825L8.45025 15.147C8.1663 13.9436 8.02432 13.342 7.71574 12.854C7.44285 12.4226 7.07744 12.0571 6.64596 11.7843C6.15803 11.4757 5.55636 11.3337 4.35301 11.0497L1.11751 10.2863C0.454433 10.1298 0.122896 10.0516 0.0482295 9.92576C-0.0160765 9.81741 -0.0160765 9.68259 0.0482295 9.57424C0.122896 9.44843 0.454434 9.3702 1.11751 9.21373L4.35302 8.45025C5.55636 8.1663 6.15803 8.02432 6.64596 7.71574C7.07744 7.44285 7.44285 7.07744 7.71574 6.64596C8.02432 6.15803 8.1663 5.55636 8.45025 4.35301L9.21373 1.11751Z"
+                                            fill="black"
+                                        />
+                                    </svg>
+
                                     <p className={styles.direction__text}>{dir}</p>
                                 </div>
                             ))}
@@ -270,7 +284,7 @@ export default function CoursePage() {
                         <h2 className={styles.cta__title}>Начните путь к новому телу</h2>
 
                         {/* Исправлена проверка benefits */}
-                        <ul className={styles.benefits}>
+                        <ul className={styles.cta__benefits}>
                             {(course.benefits?.length
                                 ? course.benefits
                                 : [
@@ -285,6 +299,7 @@ export default function CoursePage() {
                             ))}
                         </ul>
 
+                        {/* Кнопки */}
                         {!user ? (
                             <button
                                 onClick={handleAddCourse}
@@ -306,33 +321,89 @@ export default function CoursePage() {
                         )}
                     </div>
 
-                    <div
-                        className={styles.cta__imagesContainer}
-                        style={{
-                            backgroundImage: `url(/img/runner-vector.png)`,
-                            backgroundSize: "auto 110%",
-                            backgroundPosition: "-470px top",
-                            backgroundRepeat: "no-repeat",
-                        }}
-                    >
+                    {/* Блок с бегуном */}
+                    <div className={styles.cta__imagesContainer}>
                         <Image
                             src="/img/runner.png"
                             alt="Бегун"
-                            width={600}
-                            height={600}
+                            width={487}
+                            height={543}
                             priority
                             className={`${styles.cta__image} ${styles.img1}`}
                         />
 
-                        <Image
-                            src="/img/runner-blue.png"
-                            alt="Бегун синий фон"
-                            width={479}
-                            height={542}
-                            priority
+                        <svg
+                            width="629"
+                            height="467"
+                            viewBox="0 0 629 420"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
                             className={`${styles.cta__image} ${styles.img2}`}
-                        />
+                        >
+                            <path
+                                d="M499.782 388.796C943.782 -230.705 47.9024 39.5477 24.1395 237.96C18.5256 284.834 82.2022 298.667 177.705 277.697C273.208 256.727 -131.258 411.114 54.5178 439.481"
+                                stroke="#C6FF00"
+                                stroke-width="10.1395"
+                            />
+                        </svg>
+
+                        <svg
+                            width="54"
+                            height="47"
+                            viewBox="0 0 54 47"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className={`${styles.cta__image} ${styles.img3}`}
+                        >
+                            <path
+                                d="M2.82886 45.271C5.82886 36.771 20.0289 16.371 52.8289 2.771"
+                                stroke="black"
+                                stroke-width="6"
+                            />
+                        </svg>
                     </div>
+                </section>
+
+                {/* Блок с бегуном для мобилки */}
+                <section className={styles.cta__imagesContainerMob}>
+                    <Image
+                        src="/img/runner.png"
+                        alt="Бегун"
+                        width={313}
+                        height={352}
+                        priority
+                        className={`${styles.cta__imageMob} ${styles.img1Mob}`}
+                    />
+
+                    <svg
+                        width="629"
+                        height="467"
+                        viewBox="0 0 629 420"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`${styles.cta__imageMob} ${styles.img2Mob}`}
+                    >
+                        <path
+                            d="M499.782 388.796C943.782 -230.705 47.9024 39.5477 24.1395 237.96C18.5256 284.834 82.2022 298.667 177.705 277.697C273.208 256.727 -131.258 411.114 54.5178 439.481"
+                            stroke="#C6FF00"
+                            stroke-width="10.1395"
+                        />
+                    </svg>
+
+                    <svg
+                        width="54"
+                        height="47"
+                        viewBox="0 0 54 47"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`${styles.cta__imageMob} ${styles.img3Mob}`}
+                    >
+                        <path
+                            d="M2.82886 45.271C5.82886 36.771 20.0289 16.371 52.8289 2.771"
+                            stroke="black"
+                            stroke-width="6"
+                        />
+                    </svg>
                 </section>
 
                 {/* Toast в конце */}
